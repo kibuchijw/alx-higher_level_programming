11fef6ed69b14a9ed17654c76e30e4f6576bacf4
kibuchijw <kibuchijw@gmail.com>
2023-08-15 17:25:08 +0000

    Improved .format implementation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        my_list = []
     for reversed_list in reversed(my_list):
-        print("{}".format(reversed_list))
+        print("{:d}".format(reversed_list))
