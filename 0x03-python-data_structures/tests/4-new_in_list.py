6c3b3079708fbc7d399776d6bc42af32d3c6d840
kibuchijw <kibuchijw@gmail.com>
2023-08-15 02:25:56 +0000

    Fixed relational operator issue in 4-new_in_list

--- 0x03-python-data_structures/tests/4-new_in_list.py
+++ 0x03-python-data_structures/tests/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 >= idx < len(my_list):
+    if 0 <= idx < len(my_list):
         return my_list
     list_copy = [i for i in my_list]
     list_copy[idx] = element
