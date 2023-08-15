86161a074bc90454c9037d3e9cf4d803739cc3e1
kibuchijw <kibuchijw@gmail.com>
2023-08-15 02:42:40 +0000

    Improved 8-multiple_returns

--- 0x03-python-data_structures/tests/8-multiple_returns.py
+++ 0x03-python-data_structures/tests/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return length, first
+    if sentence == '':
+        return 0, None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        return length, first
