1b54c711cb324c1dec81f5a2b8a33eb26ab13828
kibuchijw <kibuchijw@gmail.com>
2023-08-15 02:34:50 +0000

    Reverted changes

--- 0x03-python-data_structures/tests/4-new_in_list.py
+++ 0x03-python-data_structures/tests/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
+    if 0 >= idx < len(my_list):
         return my_list
+
     list_copy = [i for i in my_list]
     list_copy[idx] = element
     return list_copy
