6d338a0f2b9199e1072146260c38e610752afd1f
kibuchijw <kibuchijw@gmail.com>
2023-08-18 02:42:18 +0000

    Changed implementation in 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = list(map(lambda x: x ** 2 row:
-                              list(map(squared, row)), matrix))
-
-    return squared_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
