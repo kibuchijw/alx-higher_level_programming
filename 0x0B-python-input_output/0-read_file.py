664af176718c8f71e8293c20291ba912cf459a0e
kibuchijw <kibuchijw@gmail.com>
2023-09-17 10:02:46 +0000

    Function that reads a text file and print to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Read and print content of a text file(UTF-8) to stdout
+param filename: Name of file to read
+type filename: str
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads and prints contents of a text file
+    """
+
+    with open(filename, "r", encoding="utf-8") as file:
+                content = file.read()
+                print(content)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,4 @@
+## Python - Input/Output
+* A comprehensive dive into the python file operations
+### Read file
+* A function that reads a text file (`UTF8`) and prints it to stdout
