f624714afc28cd3b41a02d92db6019377a8813b1
kibuchijw <kibuchijw@gmail.com>
2023-11-19 13:56:09 +0000

    Fix '7-model_state_fetch_all.py', styling issue

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     # Create a Session object
@@ -23,4 +25,3 @@ if __name__ == "__main__":
 
     # Close the Session
     session.close()
-
