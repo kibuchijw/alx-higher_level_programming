98ca01ec5d05066103a5c9e1a6e3a033a3bdf1eb
kibuchijw <kibuchijw@gmail.com>
2023-11-19 11:44:16 +0000

    Fix styling for 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Lists all states with a name starting
-with N(upper N) from the  database hbtn_0e_0_usa 
+with N(upper N) from the  database hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
