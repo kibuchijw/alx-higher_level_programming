a75f5ebd5644f2087f2a5b07f0e67fc4bda7266d
kibuchijw <kibuchijw@gmail.com>
2023-11-20 01:49:50 +0000

    Add (12-mode_state_update_id_2.py), a script that changes the name of a 'State' object from the database 'hbtn_0e_6_usa'

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Adds the 'State' object "Louisiana" to the database 'hbtn_0e_6_usa'
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Connect to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a Session object
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the State object with id 2
+    state_to_update = session.query(State).get(2)
+
+    # Change the state's name to "New Mexico"
+    state_to_update.name = "New Mexico"
+
+    # Commit the changes to the database
+    session.commit()
+
+    # Close the Session
+    session.close()
