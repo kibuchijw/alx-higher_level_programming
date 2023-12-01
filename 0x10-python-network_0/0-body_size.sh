33f3222f26937766258cc17f34f1e31938bb43d0
kibuchijw <kibuchijw@gmail.com>
2023-12-01 09:52:25 +0000

    Refactor (0-body_size.sh), to meet task requirements: 'bash script should be exactly three lines long'

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL=$1
-
 # Send a GET request using curl and display the size of the response body in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$URL"
+curl -s "$1" | wc -c
