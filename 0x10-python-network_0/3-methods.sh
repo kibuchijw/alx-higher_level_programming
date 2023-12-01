b7df568f335f9ffb9e3b683188a49ae3842e22fc
kibuchijw <kibuchijw@gmail.com>
2023-12-01 10:48:32 +0000

    Fix (3-methods.sh), missing paramater)

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept
-curl -siX OPTIONS | grep "Allow" | cut -d " " -f 2-
+curl -siX OPTIONS "$1" | grep "Allow" | cut -d " " -f 2-
