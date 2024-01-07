17f32700e21ba5b2e1e34fdce34d1b706fac6a81
kibuchijw <kibuchijw@gmail.com>
2024-01-07 16:56:02 +0300

    Update (102-magic_calculation.py), solve indent issue

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,6 +7,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):  # Iterate from 4 to 5 (inclusive)
             c = add(c, i)
-            return c
+        return c
     else:
         return sub(a, b)
