06d25a7370232bdca42d905b15b6d53e8984285f
kibuchijw <kibuchijw@gmail.com>
2024-01-07 16:40:17 +0300

    Add (102-magic_calculation.py), a Python function def magic_calculation(a, b): that does exactly the Python bytecode given

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+   """Performs calculations based on the given bytecode instructions."""
+
+   from magic_calculation_102 import add, sub  # Import functions from module
+
+   if a < b:
+       c = add(a, b)
+       for i in range(4, 6):  # Iterate from 4 to 5 (inclusive)
+           c = add(c, i)
+       return c
+   else:
+       return sub(a, b)
