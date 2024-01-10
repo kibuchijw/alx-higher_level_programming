32b1adb08de20959e97f6dda2bc2248d88c3a82d
kibuchijw <kibuchijw@gmail.com>
2024-01-10 22:50:27 +0300

    Update 2-script.js to follow semistandard rules

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 // jQuery clieck event handler for DIV#red_header
 $('red_header').click(function () {
-	// Update text color of <header> to red when DIV#red_header is clicked
-	$('header').css('color', '#FF0000');
+  // Update text color of <header> to red when DIV#red_header is clicked
+  $('header').css('color', '#FF0000');
 });
