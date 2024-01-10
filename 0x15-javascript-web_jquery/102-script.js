a117b3d673a18f20e6dee43380f772cc24f76c75
kibuchijw <kibuchijw@gmail.com>
2024-01-11 01:07:37 +0300

    Fix semistandard errors

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // jQuery click event handler for fetching translation
-  $('#btn_translate').click(function() {
+  $('#btn_translate').click(function () {
     const url = 'https://www.fourtonfish.com/hellosalut/?';
     // jQuery AJAX GET request to fetch translation
     $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
